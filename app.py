5134cfb9dad85bb1eba207e45f8474dc48238788
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-04 14:06:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from trulens_eval import Tru, Feedback, Select
+from trulens_eval.feedback import Groundedness
+from trulens_eval.feedback.provider.openai import OpenAI as fOpenAI
+import numpy as np
 
 st.title("🦜🔗 Langchain Quickstart App")
 
+# Sidebar for OpenAI API key input
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    st.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
 
+# Check if OpenAI API key is provided
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+else:
+    # Langchain LLMS function for generating responses
+    def generate_response(input_text):
+        llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+        st.info(llm(input_text))
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    # Streamlit form for input text and response generation
+    with st.form("my_form"):
+        text = st.text_area("Enter text:", "What are 3 key pieces of advice for learning how to code?")
+        submitted = st.form_submit_button("Submit")
 
+        # Check if the form is submitted
+        if submitted:
+            generate_response(text)
 
-with st.form("my_form"):
-    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-    elif submitted:
-        generate_response(text)
+# TruLens Quickstart code (without running it)
+# ... (Your TruLens Quickstart code here)
 
+# Additional Streamlit components or visualization can be added here
